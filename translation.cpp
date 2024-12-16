1ddd09d0ba8f636078cce3801bb04c2060c775d2
YuniWulan <81033930+YuniWulan@users.noreply.github.com>
2024-12-16 22:41:57 +0700

    Update translation.cpp

--- translation.cpp
+++ translation.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
- 
+
 using namespace std;
- 
+
 int main()
 {
-    string a, b;
-    cin >> a >> b;
-    reverse(b.begin(), b.end());
-    if (a == b)
+    string a, b; // Deklarasi dua string `a` dan `b`
+    cin >> a >> b; // Membaca input string `a` dan `b`
+    reverse(b.begin(), b.end()); // Membalik string `b` (menggunakan fungsi `reverse`)
+    if (a == b) // Memeriksa apakah string `a` sama dengan string `b` setelah dibalik
     {
-        cout << "YES" << endl;
+        cout << "YES" << endl; // Jika sama, cetak "YES"
     }
     else
     {
-        cout << "NO" << endl;
+        cout << "NO" << endl; // Jika tidak sama, cetak "NO"
     }
- 
+
     return 0;
 }
